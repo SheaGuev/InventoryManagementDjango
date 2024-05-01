0afffa992afeb5c9c715a10d3e7bebdb1bee4ae7
Mr-Harsh14 <126354475+Mr-Harsh14@users.noreply.github.com>
2024-05-01 22:37:11 +0100

    merging

---
+++ authentication/forms.py
@@ -0,0 +1,51 @@
+from django import forms
+from django.contrib.auth.forms import UserCreationForm
+from .models import CustomUser
+
+class NewUserForm(UserCreationForm):
+    email = forms.EmailField(
+        required=True,
+        error_messages={'required': ''},
+        help_text='',
+    )
+    first_name = forms.CharField(
+        required=True,
+        error_messages={'required': ''},
+        help_text='',
+    )
+    last_name = forms.CharField(
+        required=True,
+        error_messages={'required': ''},
+        help_text='',
+    )
+    phone = forms.CharField(
+        required=True,
+        error_messages={'required': ''},
+        help_text='',
+    )
+    # password1 = forms.CharField(
+    #     widget=forms.PasswordInput,
+    #     error_messages={'required': ''},
+    #     help_text='',
+    # )
+    password2 = forms.CharField(
+        widget=forms.PasswordInput,
+        error_messages={'required': ''},
+        help_text='',
+    )
+
+    class Meta:
+        model = CustomUser
+        fields = ("email", "first_name", "last_name", "phone", "password1", "password2")
+
+    def save(self, commit=True):
+        user = super(NewUserForm, self).save(commit=False)
+        user.email = self.cleaned_data['email']
+        user.phone = self.cleaned_data['phone']
+        if commit:
+            user.save()
+        return user
+
+class LoginForm(forms.Form):
+    email = forms.EmailField()
+    password = forms.CharField(widget=forms.PasswordInput)
---
+++ authentication/urls.py
@@ -0,0 +1,12 @@
+from django.urls import path
+from . import views
+
+urlpatterns = [
+    path('register/', views.register_request, name="register"),
+    path('', views.login_request, name="login"),
+    path('placeholder/', views.placeholder, name='placeholder'),
+    path('list_users/', views.list_users, name='list_users'),
+    path('admin_home/', views.admin_home, name='admin_home'),
+    path('user_home/', views.user_home, name='user_home'),
+    # other paths...
+]
